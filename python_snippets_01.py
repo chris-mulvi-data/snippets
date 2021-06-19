d04314add5579661e19b61e04f06068ed9bec79c
Chris Mulvihill <80359148+chris-mulvi-data@users.noreply.github.com>
2021-06-19 10:22:26 -0600

    Update python_snippets_01.py
    Added a simple bit of code to use for finding and removing a common prefix in a pandas series.

--- python_snippets_01.py
+++ python_snippets_01.py
@@ -1,2 +1,7 @@
 # IP address regex string
 ip_regex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
+
+
+# Finding and removing comon prefixes from a pandas series
+prefix = os.path.commonprefix(df['vector'].to_list())
+df['vector'].apply(lambda x: str.replace(x, prefix, '')
