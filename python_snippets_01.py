0d9ebe02cc8166e1d4929e79940a85df838fd61b
Chris Mulvihill <80359148+chris-mulvi-data@users.noreply.github.com>
2021-06-20 20:35:19 -0600

    Update python_snippets_01.py
    correcting documentation for the prefix finding code

--- python_snippets_01.py
+++ python_snippets_01.py
@@ -3,5 +3,8 @@ ip_regex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
 
 # Finding and removing comon prefixes from a pandas series
+# this works well if all elements in the the vectore follow the same naming
+# conventions fore each item.  If there is any difference, it will not return
+# the prifix.
 prefix = os.path.commonprefix(df['vector'].to_list())
 df['vector'].apply(lambda x: str.replace(x, prefix, '')
