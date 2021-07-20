a8f4c21ac5e9d99fb32afcb07a947b6746b6b9de
Chris <mulvihill.chris@outlook.com>
2021-07-19 20:52:02 -0600

    updating the snippets

--- python_snippets_01.py
+++ python_snippets_01.py
@@ -14,3 +14,25 @@ df['vector'].apply(lambda x: str.replace(x, prefix, '')
 df['new_vector'] = df['reference_vector'].apply(
     lambda x: dictionary[x].get('key') if x in dictionary.keys() else 'none'
 )
+
+# Function to run binary search 
+def binary_search(data, answer):
+    '''simple bynary search algorithm from scratch'''
+    # ordering the data
+    data = sorted(data)
+    # define botom index
+    low = 0
+    # define top index
+    high = len(data) - 1
+    # setting a count for the number of opperations
+    count = 0
+    while low <= high:
+        count += 1 # increment the count by 1
+        mid = round((low + high) / 2)
+        guess = data[mid]
+        if guess == answer: 
+            return [count, guess]
+        if guess > answer:
+            high = mid - 1
+        else: 
+            low = mid + 
