ab18bb130b97f3ad79a6c9326294b69c8ea00f7a
Chris Mulvihill <chris@LAPTOP-TG7N5E0P.localdomain>
2021-07-20 09:44:25 -0600

    adding new file for the bisearch and updating other files.

--- bisearch.py
+++ bisearch.py
@@ -1,21 +1,4 @@
-# IP address regex string
-ip_regex = "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"
 
-
-# Finding and removing comon prefixes from a pandas series
-# this works well if all elements in the the vectore follow the same naming
-# conventions fore each item.  If there is any difference, it will not return
-# the prifix.
-prefix = os.path.commonprefix(df['vector'].to_list())
-df['vector'].apply(lambda x: str.replace(x, prefix, '')
-
-
-# To use lookup dictionary to apply a value to a new column of a dataframe
-df['new_vector'] = df['reference_vector'].apply(
-    lambda x: dictionary[x].get('key') if x in dictionary.keys() else 'none'
-)
-
-# Function to run binary search 
 def binary_search(data, answer):
     '''
     simple bynary search algorithm from scratch
@@ -42,4 +25,4 @@ def binary_search(data, answer):
         if guess > answer:
             high = mid - 1
         else: 
-            low = mid + 
+            low = mid + 1
