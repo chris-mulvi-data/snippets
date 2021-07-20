16b5562d9a58b9fa1e79be96d68b69da6f7a8e93
Chris <mulvihill.chris@outlook.com>
2021-07-19 21:12:15 -0600

    updating snippets

--- python_snippets_01.py
+++ python_snippets_01.py
@@ -17,8 +17,12 @@ df['new_vector'] = df['reference_vector'].apply(
 
 # Function to run binary search 
 def binary_search(data, answer):
-    '''simple bynary search algorithm from scratch'''
-    # ordering the data
+    '''
+    simple bynary search algorithm from scratch
+    takes a data set in the form of a list and seachres 
+    for the provided value within the list
+    '''
+    # ordering the data 
     data = sorted(data)
     # define botom index
     low = 0
@@ -28,9 +32,12 @@ def binary_search(data, answer):
     count = 0
     while low <= high:
         count += 1 # increment the count by 1
+        # find the mid point in the sorted data
         mid = round((low + high) / 2)
+        # define the 'guess' value from the midpoint of the data
         guess = data[mid]
-        if guess == answer: 
+        # check the guess against the desired value
+        if guess == answer:  
             return [count, guess]
         if guess > answer:
             high = mid - 1
